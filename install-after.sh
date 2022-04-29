5296d04a1e3dc3a09dc672bc76d2f2175adceed2
Jari Aalto <jari.aalto@cante.net>
2022-04-29 10:52:41 +0300

    install-after.sh: (SetPythonVersion): new

--- install-after.sh
+++ install-after.sh
@@ -9,15 +9,34 @@
 #
 # This script is run after [install] command.
 
-Cmd()
+Cmd ()
 {
     echo $*
     ${test+echo} $*
 }
 
+SetPythonVersion ()
+{
+    root="$1"
+    dir=$root/usr/bin
+    file=$dir/hg
+
+    [ -f "$file" ] || return 1
+
+    # libdir = '../lib/python3.8/site-packages'
+    python=$(awk '/libdir.*=/ { gsub(/\x27/,""); print $(NF); exit}' "$file")
+    python=${python##*lib/}
+    python=${python%/*}
+
+    [ "$python" ] || return 2
+
+    sed --in-place "/#!/s,python\( \|$\),$python," $dir/*
+}
+
 Main()
 {
     root=${1:-".inst"}
+    root=${root%/}      # delete trailing slash
 
     if [ "$root"  ] && [ -d $root ]; then
 
@@ -27,6 +46,8 @@ Main()
 	Cmd install -m 0755 -d $mandir/man1 $mandir/man5
 	Cmd install -m 0644 doc/*.1 $mandir/man1
 	Cmd install -m 0644 doc/*.5 $mandir/man5
+
+        SetPythonVersion $root
 
     fi
 }
